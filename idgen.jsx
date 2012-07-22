8f0dedf601d941848dd309bb35aa85afb3f02176
Jezeniel Zapanta <jpzapanta22@gmail.com>
2012-07-22 14:14:56 +0800

    Optimized the code for better readability
    -added constant variable for settings,
    
    -rearranged functions for readability
    
    -replaced getNameWidth() and getEmailWidth()
     with just getWidth()

--- idgen.jsx
+++ idgen.jsx
@@ -1,30 +1,14 @@
 var originalUnit = app.preferences.rulerUnits;
 app.preferences.ruleUnits = Units.PIXELS;
 
-//open file paths
-var filepath = Folder.selectDialog("Select the folder where ID.psd, pictures and names.txt are:")
-var savepath = Folder.selectDialog("Select the folder where to save images:")
+/*PUT THE SETTINGS HERE*/
 
-var maxWidth = 2626 //maxwidth for name text
-var horizonScale = 100 //percentage of horizontal scale
-
-var fileRef = File(filepath + "/ID.psd");
-var docRef = app.open(fileRef);
-
-/*
-docRef.layers[1].name = "IDName"
-docRef.layers[3].name = "IDNum"
-docRef.layers[4].name = "Contact"
-docRef.layers[4].name = "Email";
-*/
-
-//reference the PSD's layers
-var nameRef = docRef.layers.getByName("IDName").textItem;
-var nameWidth = getNameWidth();
-var numRef = docRef.layers.getByName("IDNum").textItem;
-var emailRef = docRef.layers.getByName("Email").textItem;
-var contactRef = docRef.layers.getByName("Contact").textItem;
-var newPhotoFmt = docRef.layers.getByName("PhotoFormat");
+//Layer Names
+var NAME = "IDName";
+var IDNUM = "IDNum";
+var EMAIL = "Email";
+var CONTACT = "Contact";
+var PHOTOFORMAT = "PhotoFormat";
 
 //specify save options
 var jpgSaveOpt = new JPEGSaveOptions();
@@ -33,6 +17,87 @@ jpgSaveOpt.embedColorProfile = true;
 jpgSaveOpt.formatOptions = FormatOptions.STANDARDBASELINE;
 jpgSaveOpt.matte = MatteType.NONE;
 
+/*END OF SETTINGS */
+
+function changePhoto(photoName) {
+	//opens the new photo
+	var fileRef2 = File(photoName);
+	var newPicRef = app.open(fileRef2);
+	
+	newPicRef.selection.selectAll();
+	newPicRef.selection.copy();
+	newPicRef.close(SaveOptions.DONOTSAVECHANGES);
+	
+	app.activeDocument = docRef;
+	docRef.activeLayer = PhotoFmt;
+	
+	docRef.paste();
+	docRef.activeLayer.name = "Photo";
+	var newPhoto = docRef.layers.getByName("Photo");
+	
+	copyLayerStyle(PhotoFmt, newPhoto);
+	//get photoformat's location
+	var pfmtX = PhotoFmt.bounds[0];
+	var pfmtY = PhotoFmt.bounds[1];
+	//get new photo's location
+	var photoX = newPhoto.bounds[0];
+	var photoY = newPhoto.bounds[1];
+
+	//move newPhoto to photoFormats location
+	newPhoto.translate(pfmtX - photoX, pfmtY - photoY);
+	
+	PhotoFmt.visible = false;
+	
+	//save the file
+	docRef.saveAs(jpgFile,jpgSaveOpt,true, Extension.LOWERCASE);
+	newPhoto.remove();
+	
+}
+
+//replaces text to underscore
+function changeNameToJPEGName(text) {
+		var jpgName = text.replace(",", "");
+		while(jpgName.indexOf(" ") != -1) {
+			jpgName = jpgName.replace(" ", "_");
+		}
+		return jpgName;
+}
+
+function getWidth(layername) {
+		return docRef.layers.getByName(layername).bounds[2];
+}
+
+//copy layer style
+function copyLayerStyle(layer1, layer2) {
+	
+		app.activeDocument.activeLayer = layer1;
+		var id157 = charIDToTypeID("CpFX");
+		executeAction(id157, undefined, DialogModes.ALL);
+		app.activeDocument.activeLayer = layer2;
+		var id158 = charIDToTypeID("PaFX");
+		executeAction(id158, undefined, DialogModes.ALL);
+	
+	
+}
+
+
+//open file paths
+var filepath = Folder.selectDialog("Select the folder where ID.psd, pictures and names.txt are:");
+var savepath = Folder.selectDialog("Select the folder where to save images:");
+
+var maxWidth = 2626; //maxwidth for name text
+var horizonScale = 100; //percentage of horizontal scale
+
+//references the ID format
+var fileRef = File(filepath + "/ID.psd");
+var docRef = app.open(fileRef);
+
+//reference the PSD's layers
+var nameRef = docRef.layers.getByName(NAME).textItem;
+var numRef = docRef.layers.getByName(IDNUM).textItem;
+var emailRef = docRef.layers.getByName(EMAIL).textItem;
+var contactRef = docRef.layers.getByName(CONTACT).textItem;
+var PhotoFmt = docRef.layers.getByName(PHOTOFORMAT);
 
 // open text files- READONLY
 var txtNameFile = File(filepath + "/names.txt");
@@ -46,10 +111,11 @@ txtEmailFile.open('r');
 
 var txtIdNumFile = File(filepath + "/idnums.txt");
 txtIdNumFile.open('r');
+
 //loop for processing image
 while(!txtNameFile.eof){
 	horizonScale = 100;
-	newPhotoFmt.visible = true;
+	PhotoFmt.visible = true;
 	nameRef.horizontalScale = horizonScale;
 	emailRef.horizontalScale = horizonScale;
 	
@@ -73,116 +139,55 @@ while(!txtNameFile.eof){
 	numRef.contents = txtIdNum;
 	
 	//specify save path
-	jpgFile = new File(savepath + "/" + txtName + ".jpeg");
+	var jpgFile = new File(savepath + "/" + txtName + ".jpeg");
 	
 	//resize font size if width exceeds - name layer
-	nameWidth = getNameWidth();
+	nameWidth = getWidth(NAME);
 	while (nameWidth > maxWidth) {
 		horizonScale--;
 		nameRef.horizontalScale = horizonScale;
-		nameWidth = getNameWidth();
+		nameWidth = getWidth(NAME);
 	}
+
 	horizonScale = 100;
 	
 	//resize for email
-	emailWidth = getEmailWidth();
+	emailWidth = getWidth(EMAIL);
 	while(emailWidth > maxWidth) {
 		horizonScale--;
 		emailRef.horizontalScale = horizonScale;
-		emailWidth = getEmailWidth();
+		emailWidth = getWidth(EMAIL);
 	}
 
 
-	//try if file exists if not specify image
-try {
-		//change txtName spaces to underscore
-		var picName = changeNameToJPEGName(txtName);
-		changePhoto(filepath + "/" +picName +  ".jpg");
+	//try if file name with underscore exists if not try normal image name
+	try {
+			//change txtName spaces to underscore
+			var picName = changeNameToJPEGName(txtName);
+			changePhoto(filepath + "/" +picName +  ".jpg");
 	}
-catch(e) {
+	catch(e) {
+	    //if image name not found specify photo
 		try {
 			changePhoto(filepath + "/" + txtName + ".jpg");
 		}
 		catch(e) {
+			
 			try {
 				alert("ERROR " + txtName + "'s PHOTO NOT FOUND! Please specify the location.");
 				picName = File.openDialog(txtName + "'s Photo", "JPEG : *.jpg");
 				changePhoto(picName);
-			}
+				}
 			catch(e) {
 				docRef.saveAs(jpgFile,jpgSaveOpt,true, Extension.LOWERCASE);
 			}
+		
 		}
+	
 	}
+
 }
 
 app.preferences.rulerUnits = originalUnit;
 
 alert("FINISHED!!");
-
-
-function changePhoto(photoName) {
-	//opens the new photo
-	var fileRef2 = File(photoName);
-	var newPicRef = app.open(fileRef2);
-	
-	newPicRef.selection.selectAll();
-	newPicRef.selection.copy();
-	newPicRef.close(SaveOptions.DONOTSAVECHANGES);
-	
-	app.activeDocument = docRef;
-	docRef.activeLayer = newPhotoFmt;
-	
-	docRef.paste();
-	docRef.activeLayer.name = "Photo";
-	var newPhoto = docRef.layers.getByName("Photo");
-	
-	copyLayerStyle(newPhotoFmt, newPhoto);
-	//get photoformat's location
-	var pfmtX = newPhotoFmt.bounds[0];
-	var pfmtY = newPhotoFmt.bounds[1];
-	//get new photo's location
-	var photoX = newPhoto.bounds[0];
-	var photoY = newPhoto.bounds[1];
-
-	//move newPhoto to photoFormats location
-	newPhoto.translate(pfmtX - photoX, pfmtY - photoY);
-	
-	newPhotoFmt.visible = false;
-	
-	//save the file
-	docRef.saveAs(jpgFile,jpgSaveOpt,true, Extension.LOWERCASE);
-	newPhoto.remove();
-	
-};
-
-//replaces text to underscore
-function changeNameToJPEGName(text) {
-		var jpgName = text.replace(",", "");
-		while(jpgName.indexOf(" ") != -1) {
-			jpgName = jpgName.replace(" ", "_");
-		}
-		return jpgName;
-};
-
-//get name width
-function getNameWidth() {
-		return docRef.layers.getByName ("IDName").bounds[2];
-	
-} ;
-
-function getEmailWidth() {
-		return docRef.layers.getByName("Email").bounds[2];
-}
-//copy layer style
-function copyLayerStyle(layer1, layer2) {
-	
-		app.activeDocument.activeLayer = layer1;
-		var id157 = charIDToTypeID("CpFX");
-		executeAction(id157, undefined, DialogModes.ALL);
-		app,activeDocument.activeLayer = layer2;
-		var id158 = charIDToTypeID("PaFX");
-		executeAction(id158, undefined, DialogModes.ALL);
-	
-	
-};
